36f718af76bb6eebbdae4dc5396a9d91799af096
Justin Daniel <justinddaniel@gmail.com>
2017-10-30 22:48:12 +0000

    automatically backed up by learn

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,23 @@
+require 'pry'
+class FiguresController < ApplicationController
+
+  get '/figures/new' do
+    @figures = Figure.all
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    titles_array = params["figure"]["title_ids"]
+    titles_array.collect! {|a| a.to_i}
+    figure_title = Title.find_by_id(titles_array[0])
+    landmark_array = params["figure"]["landmark_ids"]
+    landmark = Landmark.find_by(name: landmark_array)
+    @figure = Figure.create(name: params["figure"]["name"])
+    @figure.titles << figure_title
+    @figure.landmarks << landmark
+    @figure.save
+  end
+
+end
